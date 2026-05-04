b67286dd0669c0cdbbbfe788837630669b89a36f
gfish <1134506645@qq.com>
2026-05-04 10:14:32 +0800

    feat: implement system tray with show/quit menu
    Task 15: Close window minimizes to tray instead of quitting.
    Left-click tray icon to show/focus window. Tray menu has
    "打开主窗口" and "退出" options.

--- src-tauri/src/tray/menu.rs
+++ src-tauri/src/tray/menu.rs
@@ -1,1 +1,27 @@
-// Tray menu will be implemented in Task 15
+use tauri::{
+    AppHandle, Manager,
+    menu::{Menu, MenuItem},
+};
+
+pub fn build_tray_menu(app: &AppHandle) -> Result<Menu<tauri::Wry>, Box<dyn std::error::Error>> {
+    let show = MenuItem::with_id(app, "show", "打开主窗口", true, None::<&str>)?;
+    let quit = MenuItem::with_id(app, "quit", "退出", true, None::<&str>)?;
+
+    let menu = Menu::with_items(app, &[&show, &quit])?;
+    Ok(menu)
+}
+
+pub fn handle_tray_menu_event(app: &AppHandle, event: tauri::menu::MenuEvent) {
+    match event.id().as_ref() {
+        "show" => {
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.show();
+                let _ = window.set_focus();
+            }
+        }
+        "quit" => {
+            app.exit(0);
+        }
+        _ => {}
+    }
+}
--- src-tauri/src/tray/mod.rs
+++ src-tauri/src/tray/mod.rs
@@ -1,6 +1,34 @@
 pub mod menu;
 
-// Tray setup will be implemented in Task 15
-pub fn setup_tray(_app: &tauri::AppHandle) -> Result<(), Box<dyn std::error::Error>> {
+use tauri::{
+    AppHandle, Manager,
+    tray::{TrayIconBuilder, TrayIconEvent, MouseButton, MouseButtonState},
+};
+
+pub fn setup_tray(app: &AppHandle) -> Result<(), Box<dyn std::error::Error>> {
+    let tray_menu = menu::build_tray_menu(app)?;
+
+    let _tray = TrayIconBuilder::new()
+        .icon(app.default_window_icon().unwrap().clone())
+        .menu(&tray_menu)
+        .on_menu_event(move |app, event| {
+            menu::handle_tray_menu_event(app, event);
+        })
+        .on_tray_icon_event(|tray, event| {
+            if let TrayIconEvent::Click {
+                button: MouseButton::Left,
+                button_state: MouseButtonState::Up,
+                ..
+            } = event
+            {
+                let app = tray.app_handle();
+                if let Some(window) = app.get_webview_window("main") {
+                    let _ = window.show();
+                    let _ = window.set_focus();
+                }
+            }
+        })
+        .build(app)?;
+
     Ok(())
 }
